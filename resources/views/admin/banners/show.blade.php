db0a1580b218ec7dc7b14f888fdddf9831e17a2a
Ana Claudia <anacnogueira@gmail.com>
2024-07-22 21:31:26 -0300

    Mostrar botões em mostrar detalhes

--- resources/views/admin/banners/show.blade.php
+++ resources/views/admin/banners/show.blade.php
@@ -6,6 +6,8 @@
     <h1>Visualizar Banner</h1>
 @stop
 
+@section('plugins.Sweetalert2', true);
+
 @section('content')    
     <div class="row">
         <div class="col-md-12">
@@ -24,7 +26,7 @@
                         <dd>{{ $banner->expire_date }}&nbsp;</dd>
                     </dl>                
                     @if (!empty($banner->html)) 
-                    {{ $banner->html }} 
+                    {!! $banner->html !!} 
                     @elseif (!empty($banner->image))
                     @if (!empty($banner->url))
                         @php
@@ -39,14 +41,26 @@
                         <img src="{{ url('images/banners/'.$banner->image) }}" >
                     </a>                    
                     @endif
-                    <div class="row">
+                    <div class="row" style="margin-top: 20px">
                         <div class="col-md-12">
-                            <div class="actions">
-                                <ul>
-                                    <li>Editar </li>
-                                    <li>Excluir</li>
-                                    <li>Listar</li>
-                                    <li>Novo Banner</li>       
+                            <div class="btn-group" style="display: inline">
+                                    <a href="{{ route('admin.banners.edit', $banner->id) }}" class="btn btn-success">
+                                        <i class="fa fa-pen"></i> Editar
+                                    </a>
+                                    <form action="{{ route('admin.banners.destroy', $banner->id) }}" method="POST" class="frm-delete" style="display: inline">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <button type="submit" class="btn btn-danger" title="Delete">
+                                            <i class="fa fa-trash"></i>  Excluir
+                                        </button>
+                                    </form>
+                                   
+                                    <a href="{{ route('admin.banners.index') }}" class="btn btn-warning">
+                                        <i class="fa fa-list-alt"></i> Listar
+                                    </a>
+                                    <a href="{{ route('admin.banners.create') }}" class="btn btn-primary">
+                                        <i class="fa fa-file"></i> Adicionar
+                                    </a>
                                 </ul>
                             </div>
                         </div>    
@@ -56,3 +70,7 @@
         </div>
     </div>    
 @stop
+
+@push('js')
+    <script  type="text/javascript" src="{{ asset('js/admin/banner-delete.js') }}"></script>
+@endpush
