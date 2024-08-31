35016a24cc1007bc18979c6da19d2104fe591d43
Ana Claudia <anacnogueira@gmail.com>
2024-08-31 13:17:51 -0300

    Adiciona Controller de formas de pagamento

---
+++ app/Http/Controllers/Admin/PaymentMethodController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Services\PaymentMethodService;
+use App\Http\Requests\AdminStoreUpdatePaymentMethodRequest;
+
+class PaymentMethodController extends Controller
+{
+    protected $paymentMethodService;
+
+    public function __construct(PaymentMethodService $paymentMethodService)
+    {
+        $this->paymentMethodService = $paymentMethodService;
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $paymentMethods = $this->paymentMethodService->getAllPaymentMethods();
+
+        return view('admin.payment-methods.index', compact('paymentMethods'));
+    }
+
+     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $paymentMethod = null;
+        
+        return view('admin.payment-methods.create', compact('paymentMethod'));
+    }
+
+     /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(AdminStoreUpdatePaymentMethodRequest $request)
+    {
+        $data = $request->all();
+        
+        $paymentMethod = $this->paymentMethodService->makePaymentMethod($data);
+
+        return redirect()->route('admin.payment-methods.index');
+    }
+
+     /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $paymentMethod = $this->paymentMethodService->getPaymentMethodById($id);
+        
+        return view('admin.payment-methods.show', compact('paymentMethod'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $paymentMethod = $this->paymentMethodService->getPaymentMethodById($id);
+        
+        return view('admin.payment-methods.edit', compact('paymentMethod'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(AdminStoreUpdatePaymentMethodRequest $request, $id)
+    {
+        $data = $request->all();
+ 
+        $paymentMethod = $this->paymentMethodService->updatePaymentMethod($id, $data);
+
+        return redirect()->route('admin.payment-methods.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $paymentMethod = $this->paymentMethodService->destroyPaymentMethod($id);
+
+        return redirect()->route('admin.payment-methods.index');
+    }
+}
