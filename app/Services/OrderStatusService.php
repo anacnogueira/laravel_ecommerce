c76a507175d0b2b97983061a072a115e4aa5c8ae
Ana Claudia <anacnogueira@gmail.com>
2024-09-13 17:11:08 -0300

    Serviço de status do pedido

---
+++ app/Services/OrderStatusService.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\OrderStatusdRepositoryInterface;
+
+class OrderStatusService
+{
+    protected $orderstatusdRepository;
+
+    public function __construct(OrderStatusdRepositoryInterface $orderStatusdRepository)
+    {
+        $this->orderStatusdRepository = $orderStatusdRepository;
+    }
+
+    /**
+     * Select all orderstatusds
+     * @return array
+    */
+    public function getAllOrderStatuses()
+    {
+        return $this->orderStatusdRepository->getAllOrderStatuses();
+    }
+
+     /**
+     * Create a new orderstatusd
+     * @param array $data
+     * @return object 
+    */
+    public function makeOrderStatusd(array $data, $file)
+    {
+        $orderStatus = $this->orderStatusRepository->createOrderStatus($data);        
+
+        return $orderStatus;
+    }
+
+    /**
+     * Get OrderStatusd by  ID
+     * @param int $id
+     * @return object
+    */
+    public function getOrderStatusById(int $id)
+    {
+        return $this->orderStatusRepository->getOrderStatusById($id);
+    }
+
+    /**
+     * Update a orderstatusd
+     * @param int $id
+     * @param arrray $data
+     * @return json response
+    */
+    public function updateOrderStatus(int $id, array $data)
+    {
+   
+        $orderStatus = $this->orderStatusRepository->getOrderStatusById($id);
+
+        if (!$orderstatus) {
+            return response()->json(['message' => 'Order Status Not Found'], 404);
+        }
+
+        $this->orderStatusRepository->updateOrderStatus($orderStatus, $data);
+        return response()->json(['message' => 'Payment Method Updated'], 200);
+    }
+
+    /**
+     * Delete a orderstatusd
+     * @param int $id
+     * @return json response
+    */
+    public function destroyOrderStatus(int $id)
+    {
+        $orderStatus = $this->orderStatusRepository->getOrderStatusById($id);
+
+        if (!$orderStatus) {
+            return response()->json(['message' => 'Order Status Not Found'], 404);
+        }
+
+        $this->orderStatusRepository->destroyOrderStatus($orderStatus);
+
+        return response()->json(['message' => 'Order Status Deleted'], 200);
+    }
+}
