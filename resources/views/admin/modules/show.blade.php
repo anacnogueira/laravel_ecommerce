1db7c01b072fe49b4c695fea8a63c22910d783cd
Ana Claudia <anacnogueira@gmail.com>
2024-09-16 17:53:27 -0300

    Visualiza módulo

---
+++ resources/views/admin/modules/show.blade.php
@@ -0,0 +1,63 @@
+@extends('adminlte::page')
+
+@section('title', 'Visualizar Módulo')
+
+@section('content_header')
+    <h1>Visualizar Módulo</h1>
+@stop
+
+@section('plugins.Sweetalert2', true);
+
+@section('content')    
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary card-outline">
+                <div class="card-body">
+                    <dl>
+                        <dt>ID:</dt>
+                        <dd>{{ $module->id }}&nbsp;</dd>
+                        <dt>Pai:</dt>
+                        <dd>{{ optional($module->parent)->name }}&nbsp;</dd>
+                        <dt>Nome:</dt>
+                        <dd>{{ $module->name }}&nbsp;</dd>
+                        <dt>Apelido:</dt>
+                        <dd>{{ $module->slug }}&nbsp;</dd>
+                        <dt>Ordem:</dt>
+                        <dd>{{ $module->order }}&nbsp;</dd>
+                        <dt>Status:</dt>
+                        <dd>{{ $module->status }}&nbsp;</dd>
+                    </dl>                
+                   
+                    <div class="row" style="margin-top: 20px">
+                        <div class="col-md-12">
+                            <div class="btn-group" style="display: inline">
+                                    <a href="{{ route('admin.modules.edit', $module->id) }}" class="btn btn-success">
+                                        <i class="fa fa-pen"></i> Editar
+                                    </a>
+                                    <form action="{{ route('admin.modules.destroy', $module->id) }}" method="POST" class="frm-delete" style="display: inline">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <button type="submit" class="btn btn-danger" title="Delete">
+                                            <i class="fa fa-trash"></i>  Excluir
+                                        </button>
+                                    </form>
+                                   
+                                    <a href="{{ route('admin.modules.index') }}" class="btn btn-warning">
+                                        <i class="fa fa-list-alt"></i> Listar
+                                    </a>
+                                    <a href="{{ route('admin.modules.create') }}" class="btn btn-primary">
+                                        <i class="fa fa-file"></i> Adicionar
+                                    </a>
+                                </ul>
+                            </div>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+@stop
+
+@push('js')
+    <script  type="text/javascript" src="{{ asset('js/admin/btn-delete.js') }}"></script>
+@endpush
