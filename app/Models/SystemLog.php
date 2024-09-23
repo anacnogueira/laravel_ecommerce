9262516a1cde7f6a9e4c1aa4550adfc3139902cf
Ana Claudia <anacnogueira@gmail.com>
2024-09-23 16:49:50 -0300

    Lista logs das ultimas 24 horas

--- app/Models/SystemLog.php
+++ app/Models/SystemLog.php
@@ -18,6 +18,49 @@ class SystemLog extends Model
         'description',   
     ];
 
+    public function module()
+    {
+        return $this->belongsTo(Module::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+     /**
+     * Get the scheduled date
+     *
+     * @param  string  scheduled date
+     * @return string
+     */
+    public function getCreatedAttribute($value)
+    {
+        return $value ? Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('d/m/Y H:i') : null;
+    }
+
+     /**
+     * Get the scheduled date
+     *
+     * @param  string  scheduled date
+     * @return string
+     */
+    public function getOperationAttribute($value)
+    {
+        switch($value) {
+            case 'create':
+                return 'Criação';
+           case 'update':
+                return 'Atualização';
+            case 'delete':
+                return 'Exclusão';
+            default:
+                return 'Desconhecida';        
+        }
+    }
+
+
     /**
      * Scope a query to only include 24 hours of logs.
      *
